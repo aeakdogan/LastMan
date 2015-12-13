59d4854165527b6c957570a8b3bb685b28fdf250
YaseminDoganci <doganci.yase@gmail.com>
2015-12-13 14:54:51 +0200

    ResultsView class is created
    will be filled after endGame of GameController is written

---
+++ LastMan/src/ResultsView.java
@@ -0,0 +1,70 @@
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.AncestorListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.JButton;
+
+
+public class ResultsView {
+
+	private JPanel displayPanel; 
+	private JButton goBackToMainButton; 
+	
+	
+	public ResultsScreen(String gameData){
+		
+		
+	}
+	
+	public void updateView(){
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent((java.awt.Graphics) g);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+		
+	}
+	
+	
+	public JPanel getDisplayPanel(){
+		return displayPanel; 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
