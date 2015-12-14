7833e63d66cbe337d46436e4102b0ba427a0bbca
Burcu <burcucan99@gmail.com>
2015-12-14 19:54:25 +0200

    settings volume added

--- LastMan/src/GameView.java
+++ LastMan/src/GameView.java
@@ -18,6 +18,7 @@ import java.util.TimerTask;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
@@ -273,6 +274,7 @@ public class GameView extends JPanel implements ActionListener
 		sound.musicStart();		
 	}
 	
+	//http://helpdesk.objects.com.au/java/how-to-control-volume-of-audio-clip
 	//http://stackoverflow.com/questions/23986793/java-playing-a-sound-on-top-of-background-music
 	private class Sound
 	{
@@ -285,7 +287,11 @@ public class GameView extends JPanel implements ActionListener
             {
 				clip = AudioSystem.getClip();
 				ais = AudioSystem.getAudioInputStream(new File(fileName));
-	            clip.open(ais);
+	            clip.open(ais);	        
+	            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+	            double gain = (0.10)*(frame.getSettings().getVolume());
+	            float dB = (float) (Math.log(gain) / Math.log(10.0) * 20.0);
+	            gainControl.setValue(dB);
 			} catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) 
             {
 				e.printStackTrace();
