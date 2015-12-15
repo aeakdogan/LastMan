446f4df26469780190bd5ebf04edabbbeb8f8038
YaseminDoganci <doganci.yase@gmail.com>
2015-12-15 10:58:30 +0200

    comments are added

--- LastMan/src/GameController.java
+++ LastMan/src/GameController.java
@@ -1,3 +1,10 @@
+/**
+ * @author Burcu Canakci
+ * GameController class is one of the most important classes of the project.
+ *  It is used to take user input to control the game. This class is responsible for creating and setting
+ *  up the game, to organize the game dynamics during the game, to finish a game and to direct the Player
+ *  to the Results Screen at the end of the game.
+ */
 public class GameController 
 {
 	//Properties
@@ -11,23 +18,42 @@ public class GameController
 		packTime = 0;
 	}
 
-	//Methods
+	
+	/**
+	 * @param no
+	 * @param level
+	 * This method creates the bots of the game as specified as the number and level such as: no: 3, 
+	 * level: medium.
+	 */
 	public void charactersSelected(int no, int level)
 	{
 		game.createBots(no, level);
 	}
 	
+	/**
+	 * @param heroId
+	 * This method sets the player as the player chosen and its usability of the packs is determined as 
+	 * HARD, such that the player can pick up and use the packs in the game. 
+	 */
 	public void heroSelected(String heroId)
 	{
 		Character player = new Character(heroId, Character.HARD, game);
 		game.setPlayer(player);
 	}
 	
+	/**
+	 * @param mapId
+	 * This method sets the map to the one that is chosen. 
+	 */
 	public void mapSelected(String mapId)
 	{
 		game.setGameMap(new GameMap(mapId, game));
 	}
 	
+	/**
+	 * @param t
+	 * @returns true if the chosen time is between 30 and 300 seconds, returns false otherwise.
+	 */
 	private boolean isValidTimer(int t)
 	{
 		if(t >= 30 && t <= 300)
@@ -35,6 +61,10 @@ public class GameController
 		return false;
 	}
 
+	/**
+	 * @param t
+	 * @returns true if the game time is valid and sets the game time as the decided game time.
+	 */
 	public boolean timerSelected(int t)
 	{
 		if (!isValidTimer(t))
@@ -43,6 +73,9 @@ public class GameController
 		return true;
 	}
 	
+	/**
+	 * @returns true if the game is ready and starts the game. 
+	 */
 	public boolean startGame()
 	{
 		if(game.isReady())
@@ -50,6 +83,10 @@ public class GameController
 		return false;
 	}
 	
+	/**
+	 * @param elapsed
+	 * updates the game as time passes. 
+	 */
 	public void updateGame(int elapsed)
 	{
 		if(game.getCurrentTime() + 1 == game.getMaxTime() | game.getDeadCharacters().contains(game.getPlayer()) | game.getAliveCharacters().size() == 1)
@@ -78,6 +115,10 @@ public class GameController
 		game.update(elapsed);
 	}
 	
+	/**
+	 * This method is used for determining the players and bots' status at the end of the game. It is used
+	 * for viewing the results screen and has the information needed for printing the result of the game. 
+	 */
 	public void endGame()
 	{
 		String resultScreenInfo = "";
